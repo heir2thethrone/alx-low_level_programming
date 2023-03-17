c1f21be770365b8c33189862f7ca471b166bc6c0
Sean Dida <didainclub@yahoo.com>
2023-03-17 15:33:56 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - Print a string
- * Description: use printf
- * Return: 0
+ * main - Entry point
+ *
+ * printf: This function display the specified string in the output
+ * Return: Returns 0 (success)
  */
+
 int main(void)
 {
-	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
